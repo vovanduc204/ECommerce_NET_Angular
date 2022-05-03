c2a82636904454a70326a0e365ceff24a6c078f0
ducvv <ducvv@ames.edu.vn>
2022-05-03 15:07:11 +0700

    Sort and Around the decimal problem

--- non-stop/API/Controllers/ProductsController.cs
+++ non-stop/API/Controllers/ProductsController.cs
@@ -34,9 +34,9 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<List<ProductToReturnDto>>> GetProducts(string sort)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification();
+            var spec = new ProductsWithTypesAndBrandsSpecification(sort);
             var products = await _productsRepo.ListAsync(spec);
 
             return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
