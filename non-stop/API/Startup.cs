7fabb3c379feb4d054de6d3ff14f8f53c6be6575
ducvv <ducvv@ames.edu.vn>
2022-05-03 09:18:42 +0700

    Configuration AutoMapper and add forder Helpers

--- non-stop/API/Startup.cs
+++ non-stop/API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Helpers;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Infrastructure.Entities;
@@ -33,6 +34,8 @@ namespace API
             services.AddScoped<IProductRepository, ProductRepository>();
 
             services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+
+            services.AddAutoMapper(typeof(MappingProfiles));
 
 
             services.AddDbContext<StoreContext>(x =>
