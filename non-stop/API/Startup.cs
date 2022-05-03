89e870bf40932edd3be69290b6d527b4ff1ec00d
ducvv <ducvv@ames.edu.vn>
2022-05-03 14:14:54 +0700

    ApplicationServicesExtensions, SwaggerServicesExtensions, Fix ProductController

--- non-stop/API/Startup.cs
+++ non-stop/API/Startup.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Errors;
+using API.Extensions;
 using API.Helpers;
 using API.Middleware;
 using Core.Interfaces;
@@ -34,55 +35,24 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddScoped<IProductRepository, ProductRepository>();
-
-            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
-
             services.AddAutoMapper(typeof(MappingProfiles));
-
 
             services.AddDbContext<StoreContext>(x =>
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
 
+            // API.Extensions -> ApplicationServicesExtensions -> AddApplicationServices
+            services.AddApplicationServices();
 
-            services.Configure<ApiBehaviorOptions>(options =>
-           {
-               options.InvalidModelStateResponseFactory = actionContext =>
-               {
-                   var errors = actionContext.ModelState
-                       .Where(e => e.Value.Errors.Count > 0)
-                       .SelectMany(x => x.Value.Errors)
-                       .Select(x => x.ErrorMessage).ToArray();
-
-                   var errorResponse = new ApiValidationErrorResponse
-                   {
-                       Errors = errors
-                   };
-
-                   return new BadRequestObjectResult(errorResponse);
-               };
-           });
+            // API.Extensions -> ApplicationServicesExtensions -> SwaggerServiceExtensions
+            services.AddSwaggerDocumentation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
             app.UseMiddleware<ExceptionMiddleware>();
-
-            if (env.IsDevelopment())
-            {
-                // app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            }
 
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
@@ -100,6 +70,9 @@ namespace API
                 ),
                 RequestPath = "/content"
             });
+
+            // API.Extensions -> ApplicationServicesExtensions -> SwaggerServiceExtensions
+            app.UseSwaggerDocumention();
 
             app.UseEndpoints(endpoints =>
             {
